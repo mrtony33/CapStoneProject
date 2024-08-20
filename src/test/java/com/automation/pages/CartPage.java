56ecd2ffb77ae9ebba2f85212f653c9734286f8d
Antony.Joseph <Antony.Joseph@ust.com>
2024-08-20 14:38:52 +0530

    scenario size

--- src/test/java/com/automation/pages/CartPage.java
+++ src/test/java/com/automation/pages/CartPage.java
@@ -3,9 +3,16 @@ package com.automation.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class CartPage extends BasePage{
     @FindBy(xpath = "//span[text()='1/1 ITEMS SELECTED']")
     WebElement cartItem;
+
+    @FindBy(xpath = "//span[contains(text(),'Size: ')]")
+    List<WebElement> cartAddedSizes;
 
     @FindBy(xpath = "//button[text()='REMOVE']")
     WebElement removeButton;
@@ -16,6 +23,25 @@ public class CartPage extends BasePage{
     @FindBy(xpath = "//div[@class='emptyCart-base-emptyText']")
     WebElement emptyCart;
 
+    @FindBy(xpath = "//span[@class='pdp-price']")
+    WebElement pdpPrice;
+
+    @FindBy(xpath = "//div[@class='priceDetail-base-total ']/span[@class='priceDetail-base-value ']")
+    WebElement cartPrice;
+
+
+    @FindBy(xpath = "(//button[contains(@class,'size-buttons-size-button ')])[1]")
+    WebElement selectSize;
+
+    @FindBy(xpath = "//div[text()='ADD TO BAG']")
+    WebElement addToCartButton;
+
+    @FindBy(xpath = "//span[text()='GO TO BAG']")
+    WebElement goToBag;
+
+    @FindBy(xpath = "//span[contains(@class,'desktop-badge') and text()='1']")
+    WebElement cartIcon;
+
     public boolean verifyAddToCart() {
 
         return isDisplayed(cartItem);
@@ -23,10 +49,35 @@ public class CartPage extends BasePage{
 
     public void clickRemove() {
         removeButton.click();
-        confirmRemoveButton.click();;
+        confirmRemoveButton.click();
     }
 
     public boolean isItemInCartDisplayed() {
         return isDisplayed(emptyCart);
+    }
+
+    int price;
+    public void getPriceOnPdp() {
+        String currentHandle=driver.getWindowHandle();
+        for (String handle:driver.getWindowHandles()){
+            if (!handle.equals(currentHandle)){
+                driver.switchTo().window(handle);
+            }
+        }
+        String p=pdpPrice.getText().replaceAll("\\D","").trim();
+        price=Integer.parseInt(p);
+        System.out.println(price);
+        doScroll(300);
+        selectSize.click();
+        addToCartButton.click();
+        if (isDisplayed(goToBag) && isDisplayed(cartIcon)) {
+            cartIcon.click();
+        }
+    }
+    public boolean verifyCartPrice() {
+        String p=cartPrice.getText().replaceAll("\\D","").trim();
+        int price2=Integer.parseInt(p);
+        System.out.println(price2);
+        return price==price2;
     }
 }
--- src/test/java/com/automation/steps/CartSteps.java
+++ src/test/java/com/automation/steps/CartSteps.java
@@ -1,6 +1,7 @@
  package com.automation.steps;
 
 import com.automation.pages.CartPage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -21,5 +22,15 @@ public class CartSteps {
     @Then("verify that item is removed from the cart")
     public void verifyThatItemIsRemovedFromTheCart() {
         Assert.assertTrue(cartPage.isItemInCartDisplayed());
+    }
+
+    @And("verify the price in the cart page")
+    public void verifyThePriceInTheCartPage() {
+        Assert.assertTrue(cartPage.verifyCartPrice());
+    }
+
+    @And("take the price on pdp page")
+    public void takeThePriceOnPdpPage() {
+        cartPage.getPriceOnPdp();
     }
 }
