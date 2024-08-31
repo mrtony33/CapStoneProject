776cee59d21ccd5f7b314101a0a97d3f80d77e66
Antony.Joseph <Antony.Joseph@ust.com>
2024-08-31 12:55:46 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/test/java/com/automation/pages/androidPages/HomePageAndroid.java
@@ -0,0 +1,35 @@
+package com.automation.pages.androidPages;
+
+import com.automation.pages.interfaces.HomePage;
+import com.automation.utils.ConfigReader;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePageAndroid extends BasePageAndroid implements HomePage {
+    @FindBy(xpath = "//input[contains(@placeholder,'Search')]")
+    WebElement searchInput;
+    @FindBy(xpath = "//a[contains(@class,'submit')]")
+    WebElement searchButton;
+
+    @FindBy(xpath = "//div[@class='desktop-logoContainer']")
+    WebElement logo;
+
+    @FindBy(xpath = "//div[@class='desktop-actions']")
+    WebElement actions;
+
+    public void openWebsite() {
+        driver.get(ConfigReader.getProperty("base.url"));
+    }
+
+    public void searchForItem(String item) {
+        if (searchInput.isDisplayed()) {
+            searchInput.sendKeys(item);
+            searchButton.click();
+        }
+    }
+
+    public boolean isHomePageDisplayed() {
+        return  logo.isDisplayed() && actions.isDisplayed();
+    }
+
+}
